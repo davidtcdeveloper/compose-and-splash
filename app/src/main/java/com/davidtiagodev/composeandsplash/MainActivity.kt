6561a03891e0e16011f740e3393ea822aa86442e
David Tiago Conceição <davidtc.developer@gmail.com>
2021-06-10 10:02:09 -0300

    Tabs navigation initial development

--- app/src/main/java/com/davidtiagodev/composeandsplash/MainActivity.kt
+++ app/src/main/java/com/davidtiagodev/composeandsplash/MainActivity.kt
@@ -3,27 +3,74 @@ package com.davidtiagodev.composeandsplash
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import com.davidtiagodev.composeandsplash.feed.FeedViewModel
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.davidtiagodev.composeandsplash.feed.ui.FeedScreen
+import com.davidtiagodev.composeandsplash.feed.ui.PhotosTabRow
 import com.davidtiagodev.composeandsplash.ui.theme.ComposeAndSplashTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    private val viewModel: FeedViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeAndSplashTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    FeedScreen(viewModel)
-                }
+            PhotosApp()
+        }
+    }
+}
+
+@Composable
+fun PhotosApp() {
+    ComposeAndSplashTheme {
+        val allScreens = PhotoScreen.values().toList()
+        val navController = rememberNavController()
+        val backstackEntry = navController.currentBackStackEntryAsState()
+        val currentScreen = PhotoScreen.fromRoute(
+            backstackEntry.value?.destination?.route
+        )
+        Scaffold(
+            topBar = {
+                PhotosTabRow(
+                    allScreens = allScreens,
+                    onTabSelected = { screen ->
+                        navController.navigate(screen.name)
+                    },
+                    currentScreen = currentScreen,
+                )
             }
+        ) { innerPadding ->
+            PhotosNavHost(
+                navController = navController,
+                modifier = Modifier.padding(innerPadding)
+            )
+        }
+    }
+}
+
+@Composable
+fun PhotosNavHost(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = PhotoScreen.Feed.name,
+        modifier = modifier
+    ) {
+        composable(PhotoScreen.Feed.name) {
+            FeedScreen()
+        }
+        composable(PhotoScreen.Popular.name) {
+            FeedScreen()
         }
     }
 }
--- app/src/main/java/com/davidtiagodev/composeandsplash/feed/ui/FeedScreen.kt
+++ app/src/main/java/com/davidtiagodev/composeandsplash/feed/ui/FeedScreen.kt
@@ -1,23 +1,21 @@
 package com.davidtiagodev.composeandsplash.feed.ui
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.davidtiagodev.composeandsplash.feed.FeedState
 import com.davidtiagodev.composeandsplash.feed.FeedViewModel
 
 @Composable
-fun FeedScreen(viewModel: FeedViewModel) {
+fun FeedScreen() {
+    val viewModel: FeedViewModel = viewModel()
+
     val lifecycleOwner = LocalLifecycleOwner.current
     val flow = viewModel.feedFlow
     val locationFlowLifecycleAware = remember(flow, lifecycleOwner) {
@@ -26,32 +24,16 @@ fun FeedScreen(viewModel: FeedViewModel) {
 
     val state by locationFlowLifecycleAware.collectAsState(FeedState.Loading)
 
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(text = "Feed")
-                },
-                actions = {
-                    IconButton(onClick = { viewModel.refresh() }) {
-                        Icon(Icons.Filled.Refresh, contentDescription = "Refresh")
-                    }
-                }
-            )
-        }
-    ) { innerPadding ->
-        BodyContent(
-            modifier = Modifier.padding(innerPadding),
-            state = state,
-        )
-    }
+    FeedContent(
+        state = state,
+    )
 }
 
 @Composable
-fun BodyContent(modifier: Modifier, state: FeedState) {
+fun FeedContent(state: FeedState) {
     when (state) {
         is FeedState.Loaded -> {
-            Column(modifier = modifier) {
+            Column {
                 FeedList(state.items)
             }
         }
