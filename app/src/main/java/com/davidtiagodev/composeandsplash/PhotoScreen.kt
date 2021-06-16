04e93990fe506b9161b240ace1f9e3d8d3e4c1f3
David Tiago Conceição <davidtc.developer@gmail.com>
2021-06-16 09:33:24 -0300

    Removing redundant property

--- app/src/main/java/com/davidtiagodev/composeandsplash/PhotoScreen.kt
+++ app/src/main/java/com/davidtiagodev/composeandsplash/PhotoScreen.kt
@@ -3,26 +3,17 @@ package com.davidtiagodev.composeandsplash
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.People
 import androidx.compose.material.icons.filled.PhotoLibrary
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.vector.ImageVector
 
 enum class PhotoScreen(
     val icon: ImageVector,
-    val body: @Composable ((String) -> Unit) -> Unit
 ) {
     Feed(
         icon = Icons.Filled.PhotoLibrary,
-        body = { com.davidtiagodev.composeandsplash.feed.ui.FeedScreen() }
     ),
     Popular(
         icon = Icons.Filled.People,
-        body = { com.davidtiagodev.composeandsplash.feed.ui.FeedScreen() }
     );
-
-    @Composable
-    fun Content(onScreenChange: (String) -> Unit) {
-        body(onScreenChange)
-    }
 
     companion object {
         fun fromRoute(route: String?): PhotoScreen =
